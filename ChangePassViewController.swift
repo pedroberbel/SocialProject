86e24d15c2867ccdf0d6cf9a12770b3813fce3c2
Pedro Luis Berbel dos Santos <pedroberbel.br@gmail.com>
2017-03-13 14:01:07 -0300

    created new changePassword View

---
+++ ChangePassViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ChangePassViewController.swift
+//  SocialProject
+//
+//  Created by Pedro Luis Berbel dos Santos on 13/03/17.
+//  Copyright © 2017 Santosplb. All rights reserved.
+//
+
+import UIKit
+
+class ChangePassViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
